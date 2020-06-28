b3590d6ebe5157263a1d0ae46e785cb8023311e8
vika <v.sapunkova@neurobotics.ru>
2020-06-28 18:09:48 +0300

    added ApiTest

---
+++ src/test/java/test/ApiTest.java
@@ -0,0 +1,44 @@
+package test;
+
+import data.DataHelper;
+import lombok.val;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import restApi.RestApi;
+
+import static com.codeborne.selenide.Selenide.open;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ApiTest {
+    @BeforeEach
+    void setUp() {
+        open("http://localhost:8080"); }
+
+    @Test
+    public void shouldCheckStatusAPIByDebitCardWithValidData() {
+        val validCardInfo = DataHelper.getValidCardInfo();
+        final String response = RestApi.paymentFormByDebitCard(validCardInfo);
+        assertTrue(response.contains("APPROVED"), "Is true when status is approved");
+    }
+
+    @Test
+    public void shouldCheckStatusAPIByDebitCardWithInvalidData() {
+        val invalidCardInfo = DataHelper.getInvalidCardInfo();
+        final String response = RestApi.paymentFormByDebitCard(invalidCardInfo);
+        assertTrue(response.contains("DECLINED"),"Is true when status is declined" );
+    }
+
+    @Test
+    public void shouldCheckStatusAPIByCreditCardWithValidData() {
+        val validCardInfo = DataHelper.getValidCardInfo();
+        final String response = RestApi.paymentFormByCreditCard(validCardInfo);
+        assertTrue(response.contains("APPROVED"), "Is true when status is approved");
+    }
+
+    @Test
+    public void shouldCheckStatusAPIByCreditCardWithInvalidData() {
+        val invalidCardInfo = DataHelper.getInvalidCardInfo();
+        final String response = RestApi.paymentFormByCreditCard(invalidCardInfo);
+        assertTrue(response.contains("DECLINED"), "Is true when status is declined");
+    }
+}
