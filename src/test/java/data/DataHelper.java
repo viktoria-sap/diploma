738557848b0531f1cf3a6b8d3843165dd982ff78
vika <v.sapunkova@neurobotics.ru>
2020-06-28 22:10:34 +0300

    changed data

--- src/test/java/data/DataHelper.java
+++ src/test/java/data/DataHelper.java
@@ -12,31 +12,31 @@ public class DataHelper {
     }
 
     public static CardInfo getValidCardInfo() {
-        return new CardInfo("4444 4444 4444 4441", "22", "11", faker.name().fullName(), "343");
+        return new CardInfo("4444 4444 4444 4441", "21", "12", faker.name().fullName(), "564");
     }
 
     public static CardInfo getInvalidCardInfo() {
-        return new CardInfo("4444 4444 4444 4442", "22", "11", faker.name().fullName(), "343");
+        return new CardInfo("4444 4444 4444 4442", "21", "12", faker.name().fullName(), "564");
     }
 
     public static CardInfo getCardInfoWithWrongNumber() {
-        return new CardInfo("4444 4444", "22", "11", faker.name().fullName(), "343");
+        return new CardInfo("4444 4444", "21", "12", faker.name().fullName(), "564");
     }
 
     public static CardInfo getCardInfoWithWrongMonth() {
-        return new CardInfo("4444 4444 4444 4441", "3", "4", faker.name().fullName(), "343");
+        return new CardInfo("4444 4444 4444 4441", "3", "4", faker.name().fullName(), "564");
     }
 
     public static CardInfo getCardInfoWithWrongCvc() {
-        return new CardInfo("4444 4444 4444 4441", "22", "11", faker.name().fullName(), "34");
+        return new CardInfo("4444 4444 4444 4441", "21", "12", faker.name().fullName(), "56");
     }
 
     public static CardInfo getCardInfoWithWrongName() {
-        return new CardInfo("4444 4444 4444 4441", "22", "11", "45453", "34");
+        return new CardInfo("4444 4444 4444 4441", "21", "12", "45453", "56");
     }
 
     public static CardInfo getCardInfoWithoutName() {
-        return new CardInfo("4444 4444 4444 4441", "22", "11", " ", "34");
+        return new CardInfo("4444 4444 4444 4441", "22", "12", " ", "56");
     }
 
     @Value
